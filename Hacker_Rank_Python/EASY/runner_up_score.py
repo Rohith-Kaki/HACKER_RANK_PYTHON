41c410cb979d9f614ebf332c09e03853183aac27
Rohith <143797376+Rohith-Kaki@users.noreply.github.com>
2023-11-15 19:40:27 +0530

    Update runner_up_score.py

--- Hacker_Rank_Python/EASY/runner_up_score.py
+++ Hacker_Rank_Python/EASY/runner_up_score.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
     n = int(input())
-    arr = map(int, input().split())
-    array = sorted(set(arr))
+    arr = map(int, input().split()) #splits the array
+    array = sorted(set(arr))    #sorts the array
     runner_up = array[-2]
     print(runner_up)
